82864038045e799781f200156d1ba7a1e64ec2a0
Nilamjr <nilamrj07@gmail.com>
2022-08-24 21:24:08 +0530

    Heart star practicing

--- heartStar.js
+++ heartStar.js
@@ -1,21 +1,3 @@
-// let n = 6;
-// let string = "";
-// // first for lower part 
-// for(let belowRow = n; belowRow > 0 ; belowRow--){
-    
-//     for(let space = 0; space < n - belowRow; space++){
-//         string += " ";
-//     }
-
-//     for(let star = 1; star < 2 * belowRow ; star++){
-//         string += "*";
-//     }
-
-// }
-// console.log(string);
-
-
-
 // var n = 6;
 // var str = "";
 // for (let i = n / 2; i < n; i += 2) {
@@ -57,21 +39,37 @@
 // console.log(str);
 
 
+let n = 14;
+let string = "";
+
+for(let i = n / 2; i < n; i += 2){
+  for(let space1 = 1; space1 < n - i; space1 += 2){
+    string += " ";
+  }
+  for(let star1 = 1; star1 <= i; star1++){
+    string += "*";
+  }
+  for(let space2 = 1; space2 < n - i + 1; space2++){
+    string += " ";
+  }
+  for(let star1 = 1; star1 < i + 1; star1++){
+    string += "*";
+  }
 
 
-let n = 6;
-let str = "";
-
-for(let i = n/2; i < n; i += 2){
-  for(let s = 1; s < n - i; s++){
-    str += "@";
-  }
-  for(let star = 1; star < i + 1; star++){
-    str += star;
-  }
-  for(let s = 1; s < i + 1; s++){
-    str += "@";
-  }
-  str += "\n";
+  string += "\n";
 }
-console.log(str);
+
+for (let i = n; i > 0; i--) {
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
+  }
+  for (let j = 1; j < i * 2; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+
+
+console.log(string);
+

fc14e526202a83a6d5f290fb0073f9662e34290a
Nilamjr <nilamrj07@gmail.com>
2022-09-03 22:03:58 +0530

    HourGlass Pattern

---
+++ AlphabetPattern/pattern3rdSep.js
@@ -0,0 +1,39 @@
+// HourGlass Pattern 
+
+let n = 5;
+let string = "";
+
+// for(let abovepyramid = 1; abovepyramid <= n; abovepyramid++){
+//     for(let sp = 0; sp < abovepyramid; sp++){
+//         string += " ";
+//     }
+//     for(let star = 1; star <= 2 * (n - abovepyramid + 1) - 1; star++){
+//         string += String.fromCharCode(star + 64);
+//     }
+//     string += "\n";
+// }
+
+// for(let belowpyramid = 2; belowpyramid <= n; belowpyramid++){
+//     for(let sp = 0; sp <= n - belowpyramid; sp++){
+//         string += " ";
+//     }
+//     for(let star = 1; star <= 2 * belowpyramid - 1; star++){
+//         string += String.fromCharCode(star + 64);
+//     }
+//     string += "\n"; 
+// }
+
+
+// pattern 12  pascal pattern
+
+for(let abovePattern = 1; abovePattern < n; abovePattern++){
+    for(let star = ; star < ; star++){
+        
+    }
+}
+
+
+
+
+
+console.log(string);

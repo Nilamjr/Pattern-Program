f50b8ee0ec4845f25332756c26945ec568e37d72
Nilamjr <nilamrj07@gmail.com>
2022-09-09 21:07:18 +0530

    array methods Start

---
+++ Arrays/app.js
@@ -0,0 +1,20 @@
+// Nilam Rakholiya
+// 09/09/2022
+console.log("Arrays and Its fill Methods");
+
+// let arr = [],
+// length = 5;
+
+// for(let i = 0; i < length; i++){
+//     arr.push(0);
+// }
+
+// console.log(arr);
+
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+arr.fill(0);
+arr.fill(4, 5);
+arr. fill(5, 2, 9);
+console.log(arr);
